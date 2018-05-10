4021a306090e59dccfbf43ac2345ef116b76cfc6
NelsonShen <neslonshen@advmeds.com>
2018-05-10 13:34:07 +0800

    1.0.4

--- QRCodeScanner/QRCodeScanner.framework/Headers/QRCodeScanner.h
+++ QRCodeScanner/QRCodeScanner.framework/Headers/QRCodeScanner.h
@@ -17,3 +17,4 @@ FOUNDATION_EXPORT const unsigned char QRCodeScannerVersionString[];
 // In this header, you should import all the public headers of your framework using statements like #import <QRCodeScanner/PublicHeader.h>
 
 
+
