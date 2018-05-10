54c262e8bc2ffd273e70e1f1fc9fd1ac8e02dc42
NelsonShen <neslonshen@advmeds.com>
2018-05-10 11:20:10 +0800

    1.0.3

---
+++ QRCodeScanner/QRCodeScanner.framework/Headers/QRCodeScanner.h
@@ -0,0 +1,19 @@
+//
+//  QRCodeScanner.h
+//  QRCodeScanner
+//
+//  Created by 沈冠州 on 2018/5/9.
+//  Copyright © 2018年 沈冠州. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QRCodeScanner.
+FOUNDATION_EXPORT double QRCodeScannerVersionNumber;
+
+//! Project version string for QRCodeScanner.
+FOUNDATION_EXPORT const unsigned char QRCodeScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QRCodeScanner/PublicHeader.h>
+
+
